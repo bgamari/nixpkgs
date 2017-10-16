caf508fb9a3064df6d1ef54415f8863464452df6
Ben Gamari <ben@smart-cactus.org>
2017-10-15 20:20:41 -0400

    binutils: Add support for overriding default ld

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7376,6 +7376,7 @@ with pkgs;
   binutils-unwrapped = callPackage ../development/tools/misc/binutils {
     # FHS sys dirs presumably only have stuff for the build platform
     noSysDirs = (targetPlatform != buildPlatform) || noSysDirs;
+    defaultLd = config.defaultLd or null;
   };
   binutils-raw = wrapBintoolsWith {
     libc = if targetPlatform != hostPlatform then libcCross else stdenv.cc.libc;
@@ -7394,6 +7395,7 @@ with pkgs;
   binutils_nogold = lowPrio (binutils-raw.override {
     bintools = binutils-raw.bintools.override {
       gold = false;
+      defaultLd = "bfd";
     };
   });
 
