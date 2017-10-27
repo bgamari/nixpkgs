8b89e5218a37ea9f9f46eae1ce8206e796cfc91f
Ben Gamari <ben@smart-cactus.org>
2017-10-27 10:33:38 -0400

    llvm: Try cross-compiling

--- pkgs/development/compilers/llvm/4/llvm.nix
+++ pkgs/development/compilers/llvm/4/llvm.nix
@@ -1,4 +1,5 @@
 { stdenv
+, buildPackages
 , fetch
 , fetchpatch
 , cmake
@@ -25,6 +26,17 @@ let
   # Used when creating a version-suffixed symlink of libLLVM.dylib
   shortVersion = with stdenv.lib;
     concatStringsSep "." (take 2 (splitString "." release_version));
+
+  crossCompiling = stdenv.buildPlatform != stdenv.hostPlatform;
+  llvmArch =
+    let target = stdenv.targetPlatform;
+    in if target.isArm
+       then "ARM"
+       else
+       if target.isx86_64
+       then "X86"
+       else throw "unknown platform";
+
 in stdenv.mkDerivation (rec {
   name = "llvm-${version}";
 
@@ -40,7 +52,9 @@ in stdenv.mkDerivation (rec {
     ++ stdenv.lib.optional enableSharedLibraries "lib";
 
   nativeBuildInputs = [ cmake python ]
-    ++ stdenv.lib.optional enableManpages python.pkgs.sphinx;
+    ++ stdenv.lib.optional enableManpages python.pkgs.sphinx
+       # for build tablegen
+    ++ stdenv.lib.optional crossCompiling buildPackages.llvm;
 
   buildInputs = [ libxml2 libffi ]
     ++ stdenv.lib.optionals stdenv.isDarwin [ libcxxabi ];
@@ -117,6 +131,16 @@ in stdenv.mkDerivation (rec {
   ++ stdenv.lib.optionals (isDarwin) [
     "-DLLVM_ENABLE_LIBCXX=ON"
     "-DCAN_TARGET_i386=false"
+  ]
+  ++ stdenv.lib.optionals crossCompiling [
+    "-DCMAKE_CROSSCOMPILING=True"
+    "-DLLVM_TABLEGEN=${buildPackages.llvm}/tablegen"
+    "-DCLANG_TABLEGEN=${buildPackages.llvm}/tablegen"
+    "-DLLVM_DEFAULT_TARGET_TRIPLE=${stdenv.targetPlatform.config}"
+    "-DTARGET_TRIPLE=${stdenv.targetPlatform.config}"
+
+    "-DCOMPILER_RT_BUILD_SANITIZERS=OFF"
+    "-DCOMPILER_RT_BUILD_XRAY=OFF"
   ]
   ++ stdenv.lib.optionals stdenv.hostPlatform.isMusl [
     "-DLLVM_HOST_TRIPLE=${stdenv.hostPlatform.config}"
