fd911a3f158f14c283a3af47e327efa91047d109
Ben Gamari <ben@smart-cactus.org>
2017-12-18 15:40:48 -0500

    fuse: Fix cross-compilation

--- pkgs/os-specific/linux/fuse/common.nix
+++ pkgs/os-specific/linux/fuse/common.nix
@@ -30,8 +30,7 @@ in stdenv.mkDerivation rec {
 
   nativeBuildInputs = if isFuse3
     then [ meson ninja pkgconfig ]
-    else [ autoconf automake libtool ];
-  buildInputs = stdenv.lib.optional (!isFuse3) gettext;
+    else [ autoconf automake gettext libtool ];
 
   outputs = [ "out" ] ++ stdenv.lib.optional isFuse3 "common";
 
