534adf8b71e5ef94e8d14065740d3c556b3fc5c5
Ben Gamari <ben@smart-cactus.org>
2018-02-24 12:09:34 -0500

    gcc6: Mark fallthrus explicitly
    Otherwise building this fails with -Werror warnings when bootstrapping from
    gcc7.

--- pkgs/development/compilers/gcc/6/default.nix
+++ pkgs/development/compilers/gcc/6/default.nix
@@ -55,7 +55,8 @@ let version = "6.4.0";
     inherit (stdenv) buildPlatform hostPlatform targetPlatform;
 
     patches =
-      [ ../use-source-date-epoch.patch ]
+      [ ../use-source-date-epoch.patch
+        ./explicit-fallthrus.patch ]
       ++ optional (targetPlatform != hostPlatform) ../libstdc++-target.patch
       ++ optional noSysDirs ../no-sys-dirs.patch
       ++ optional langFortran ../gfortran-driving.patch
