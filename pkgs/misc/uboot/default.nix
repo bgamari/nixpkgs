a93374722652364f9860b025664e99f34a8cb16d
Ben Gamari <ben@smart-cactus.org>
2019-01-28 13:30:43 -0500

    uboot: Revert modifications

--- pkgs/misc/uboot/default.nix
+++ pkgs/misc/uboot/default.nix
@@ -85,7 +85,7 @@ in rec {
     hardeningDisable = [];
     dontStrip = false;
     extraMeta.platforms = stdenv.lib.platforms.linux;
-    extraMakeFlags = [ "HOST_TOOLS_ALL=y" "CROSS_BUILD_TOOLS=1" "NO_SDL=1" "tools" "envtools" ];
+    extraMakeFlags = [ "HOST_TOOLS_ALL=y" "CROSS_BUILD_TOOLS=1" "NO_SDL=1" "tools" ];
     postConfigure = ''
       sed -i '/CONFIG_SYS_TEXT_BASE/c\CONFIG_SYS_TEXT_BASE=0x00000000' .config
     '';
@@ -95,11 +95,7 @@ in rec {
       "tools/kwboot"
       "tools/mkenvimage"
       "tools/mkimage"
-      "tools/env/fw_printenv"
     ];
-    postInstall = ''
-      ln -s $out/bin/fw_printenv $out/bin/fw_setenv
-    '';
   };
 
   ubootA20OlinuxinoLime = buildUBoot rec {
