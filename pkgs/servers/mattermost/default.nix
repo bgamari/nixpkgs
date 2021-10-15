88b0150fcbcf2c5a257600a21677f3651c3d2d4f
Ben Gamari <ben@smart-cactus.org>
2021-10-14 21:52:26 -0400

    mattermost: 5.32.1 -> 5.35.5

--- pkgs/servers/mattermost/default.nix
+++ pkgs/servers/mattermost/default.nix
@@ -1,7 +1,7 @@
 { lib, stdenv, fetchurl, fetchFromGitHub, buildGoPackage, buildEnv }:
 
 let
-  version = "5.32.1";
+  version = "5.35.5";
 
   mattermost-server = buildGoPackage rec {
     pname = "mattermost-server";
@@ -11,7 +11,7 @@ let
       owner = "mattermost";
       repo = "mattermost-server";
       rev = "v${version}";
-      sha256 = "BssrTfkIxUbXYXIfz9i+5b4rEYSzBim+/riK78m8Bxo=";
+      sha256 = "sha256:02cc90hx8202zcwiffbc3syk20hahbh93f7caj0pg6iy4z9r0ccv";
     };
 
     goPackagePath = "github.com/mattermost/mattermost-server";
@@ -28,7 +28,7 @@ let
 
     src = fetchurl {
       url = "https://releases.mattermost.com/${version}/mattermost-${version}-linux-amd64.tar.gz";
-      sha256 = "kRerl3fYRTrotj86AIFSor3GpjhABkCmego1ms9HmkQ=";
+      sha256 = "sha256:07z334bljcxa87h3xs004xzli2dyrxq8rw9lgd9ni2j94ka6mppp";
     };
 
     installPhase = ''
