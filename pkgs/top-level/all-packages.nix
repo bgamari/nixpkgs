4eff97c0b46b3b4209688df41f14550ca018f106
Ben Gamari <ben@smart-cactus.org>
2022-01-01 11:41:51 -0500

    openfec: init at 1.4.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8199,6 +8199,8 @@ with pkgs;
     opendylan-bootstrap = opendylan_bin;
   };
 
+  openfec = callPackage ../development/libraries/openfec { };
+
   ophis = python3Packages.callPackage ../development/compilers/ophis { };
 
   opendylan_bin = callPackage ../development/compilers/opendylan/bin.nix { };
