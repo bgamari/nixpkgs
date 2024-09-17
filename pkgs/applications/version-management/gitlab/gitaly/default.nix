42801a3ca3e5845490124dffd14e4123d7aaab0f
Ben Gamari <ben@smart-cactus.org>
2024-09-17 15:56:19 -0400

    gitlab: Update to 17.2.7

--- pkgs/applications/version-management/gitlab/gitaly/default.nix
+++ pkgs/applications/version-management/gitlab/gitaly/default.nix
@@ -7,7 +7,7 @@
 }:
 
 let
-  version = "17.2.0";
+  version = "17.2.7";
   package_version = "v${lib.versions.major version}";
   gitaly_package = "gitlab.com/gitlab-org/gitaly/${package_version}";
 
@@ -21,7 +21,7 @@ let
       owner = "gitlab-org";
       repo = "gitaly";
       rev = "v${version}";
-      hash = "sha256-thQ8Gufcygt3QEbIcmkroSuAky9kJ7f7hupim87AJdE=";
+      hash = "sha256-EkrAYQc9ZQlpUIcpm37ioARlnoOeXyBrqwtJRHkTphY=";
     };
 
     vendorHash = "sha256-FqnGVRldhevJgBBvJcvGXzRaYWqSHzZiXIQmCNzJv+4=";
