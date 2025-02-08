46e4d32b51e6148c55fd1a1f538905cd0f6e398e
Ben Gamari <ben@smart-cactus.org>
2025-02-08 08:43:18 -0500

    gitlab-container-registry: Add upstream patches
    Fixes various testsuite issues.

--- pkgs/by-name/gi/gitlab-container-registry/package.nix
+++ pkgs/by-name/gi/gitlab-container-registry/package.nix
@@ -2,6 +2,7 @@
   lib,
   buildGoModule,
   fetchFromGitLab,
+  fetchurl
 }:
 
 buildGoModule rec {
@@ -16,6 +17,24 @@ buildGoModule rec {
     inherit rev;
     hash = "sha256-nsWOCKHoryRcVT79/nbWXa0wnIflEeDLro3l21D6bzc=";
   };
+
+  patches = [
+    # Fix azure driver test
+    (fetchurl {
+      url = "https://gitlab.com/gitlab-org/container-registry/-/commit/d44923eb4f4c8a00451811da5d1c1cef8d2b7b31.patch";
+      hash = "sha256-GR3SHbjK50RR4qNDpqt1trheprpNsW1xzMIiuwRA4T8=";
+    })
+    # Fix TestRegulatorEnterExit test
+    (fetchurl {
+      url = "https://gitlab.com/gitlab-org/container-registry/-/commit/2b34ea628d80718b214763d9c95451dbc4a09465.patch";
+      hash = "sha256-gRA5hcbs8KnVahRNr7ZMk6qJqxtAYFMcdofkkS7aV3E=";
+    })
+    # TestListUnprefixed is broken with the filesystem driver
+    (fetchurl {
+      url = "https://gitlab.com/gitlab-org/container-registry/-/commit/268689a2f30880b7d122469a4260ca46cbc55ccd.patch";
+      hash = "sha256-CoYDcaTGnpasAdg9b31euW3Stlt9zwCU1kUbVhRVdLE=";
+    })
+  ];
 
   vendorHash = "sha256-aKE/yr2Sh+4yw4TmpaVF84rJOI6cjs0DKY326+aXO1o=";
 
